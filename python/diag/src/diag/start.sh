6ca7cba28ae21d48e7763241b1a183feb0ea919f
Tristan Israël <5672131+TristanIsrael@users.noreply.github.com>
2026-02-15 16:10:47 +0100

    Update start.sh

--- python/diag/src/diag/start.sh
+++ python/diag/src/diag/start.sh
@@ -20,4 +20,4 @@ echo "Detected mouse at: $MOUSE_EVDEV"
 # Close the splash
 echo "exit" > /.splash.ctrl
 
-/usr/bin/python3 /usr/lib/safecor/diag/src/diag/main.py -platform linuxfb -plugin EvdevKeyboard:$KEYBOARD_EVDEV -plugin EvdevMouse:$MOUSE_EVDEV
+/usr/bin/python3 /usr/lib/safecor/diag/src/diag/main.py -platform linuxfb:tty=/dev/tty16 -plugin EvdevKeyboard:$KEYBOARD_EVDEV -plugin EvdevMouse:$MOUSE_EVDEV > /dev/null 2>&1
