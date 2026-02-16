3d04f791c74eb2e5d441570fe27769c642684855
Tristan Israël <5672131+TristanIsrael@users.noreply.github.com>
2026-02-16 11:28:58 +0100

    Update start.sh

--- python/diag/src/diag/start.sh
+++ python/diag/src/diag/start.sh
@@ -1,27 +1,29 @@
 #!/bin/sh
 
 # Restart udev
-rc-service udev-trigger restart
+#rc-service udev-trigger restart
 
 # Look for the keyboard
-KEYBOARD=$(find /dev/input/by-path/ -type l -name '*-kbd' | head -n1)
+#KEYBOARD=$(find /dev/input/by-path/ -type l -name '*-kbd' | head -n1)
 
 # Look for the mouse
-MOUSE=$(find /dev/input/by-path/ -type l -name '*-mouse' | head -n1)
+#MOUSE=$(find /dev/input/by-path/ -type l -name '*-mouse' | head -n1)
 
 # Resolve symbolic links from /dev/input/eventX
-KEYBOARD_EVDEV=$(readlink -f "$KEYBOARD")
-MOUSE_EVDEV=$(readlink -f "$MOUSE")
+#KEYBOARD_EVDEV=$(readlink -f "$KEYBOARD")
+#MOUSE_EVDEV=$(readlink -f "$MOUSE")
 
 # Affichage de debug
-echo "Detected keyboard at: $KEYBOARD_EVDEV"
-echo "Detected mouse at: $MOUSE_EVDEV"
+#echo "Detected keyboard at: $KEYBOARD_EVDEV"
+#echo "Detected mouse at: $MOUSE_EVDEV"
 
 # Disable the console cursor
 echo 0 > /sys/class/graphics/fbcon/cursor_blink 2>/dev/null || true
 
 # Close the splash
 echo "exit" > /.splash.ctrl 2>/dev/null || true
+
+sleep 1
 
 # Start the app
 /usr/bin/python3 /usr/lib/safecor/diag/src/diag/main.py -platform linuxfb -plugin EvdevKeyboard:$KEYBOARD_EVDEV -plugin EvdevMouse:$MOUSE_EVDEV
