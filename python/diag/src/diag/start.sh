f42c4ae36958d37f326ecec5c1fb0c7d26375ecc
Tristan Israël <5672131+TristanIsrael@users.noreply.github.com>
2026-02-16 12:28:52 +0100

    update

--- python/diag/src/diag/start.sh
+++ python/diag/src/diag/start.sh
@@ -4,18 +4,18 @@
 #rc-service udev-trigger restart
 
 # Look for the keyboard
-#KEYBOARD=$(find /dev/input/by-path/ -type l -name '*-kbd' | head -n1)
+KEYBOARD=$(find /dev/input/by-path/ -type l -name '*-kbd' | head -n1)
 
 # Look for the mouse
-#MOUSE=$(find /dev/input/by-path/ -type l -name '*-mouse' | head -n1)
+MOUSE=$(find /dev/input/by-path/ -type l -name '*-mouse' | head -n1)
 
 # Resolve symbolic links from /dev/input/eventX
-#KEYBOARD_EVDEV=$(readlink -f "$KEYBOARD")
-#MOUSE_EVDEV=$(readlink -f "$MOUSE")
+KEYBOARD_EVDEV=$(readlink -f "$KEYBOARD")
+MOUSE_EVDEV=$(readlink -f "$MOUSE")
 
 # Affichage de debug
-#echo "Detected keyboard at: $KEYBOARD_EVDEV"
-#echo "Detected mouse at: $MOUSE_EVDEV"
+echo "Detected keyboard at: $KEYBOARD_EVDEV"
+echo "Detected mouse at: $MOUSE_EVDEV"
 
 # Disable the console cursor
 echo 0 > /sys/class/graphics/fbcon/cursor_blink 2>/dev/null || true
