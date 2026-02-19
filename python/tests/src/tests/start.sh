ef2ec1185105f35f67327434893b70ed61c02aaa
Tristan Israël <5672131+TristanIsrael@users.noreply.github.com>
2026-02-19 20:53:39 +0100

    Fix safecor-tests

--- python/tests/src/tests/start.sh
+++ python/tests/src/tests/start.sh
@@ -1,20 +1,4 @@
 #!/bin/sh
 
-# Restart udev
-rc-service udev-trigger restart
-
-# Look for the keyboard
-KEYBOARD=$(find /dev/input/by-path/ -type l -name '*-kbd' | head -n1)
-
-# Look for the mouse
-MOUSE=$(find /dev/input/by-path/ -type l -name '*-mouse' | head -n1)
-
-# Resolve symbolic links from /dev/input/eventX
-KEYBOARD_EVDEV=$(readlink -f "$KEYBOARD")
-MOUSE_EVDEV=$(readlink -f "$MOUSE")
-
-# Affichage de debug
-echo "Detected keyboard at: $KEYBOARD_EVDEV"
-echo "Detected mouse at: $MOUSE_EVDEV"
-
-/usr/bin/python3 /usr/lib/safecor/diag/src/diag/main.py -platform linuxfb -plugin EvdevKeyboard:$KEYBOARD_EVDEV -plugin EvdevMouse:$MOUSE_EVDEV
+cd /usr/lib/safecor/tests/src/tests
+DISPLAY=:0 /usr/bin/python3 main.py -platform xcb 
