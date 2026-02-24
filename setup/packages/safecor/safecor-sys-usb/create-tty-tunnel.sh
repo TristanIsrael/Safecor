c8d8b65271dff54824594fca47fa07669df8a10b
Tristan Israël <5672131+TristanIsrael@users.noreply.github.com>
2026-02-24 23:23:49 +0100

    fixes

--- setup/packages/safecor/safecor-sys-usb/create-tty-tunnel.sh
+++ setup/packages/safecor/safecor-sys-usb/create-tty-tunnel.sh
@@ -1,12 +1,42 @@
 #!/bin/sh
 
-#if [ -e /dev/ttyUSB0 ]; then
-#    socat /dev/ttyUSB0,raw,echo=0 /dev/hvc3,raw,echo=0
-#fi
+DEBUG_ON=$(xenstore-read /local/domain/system/debug_on)
 
-for device in /dev/ttyUSB*; do
-    if [ -e "$device" ]; then 
-        echo Create TTY tunnel for "$device"
-        socat $device,raw,echo=0 /dev/hvc3,raw,echo=0 &
+# Verify whether the command worked
+if [ $? -ne 0 ]; then 
+    echo "Missing debug key in the xenstore. Assuming Debug is off."
+    exit 0
+fi 
+
+if [ "$DEBUG_ON" = "0" ]; then
+    echo "Debug is explicitely disabled. Aborting."
+    exit 0
+fi 
+
+# If the device was removed
+if [ "$ACTION" = "remove" ]; then 
+    echo "Destroying existing tunnel on $DEVNAME"
+    SOCAT_PID=$(pgrep -f "socat.*$DEVNAME")
+
+    if [ $SOCAT_PID -gt 0 ]; then 
+        echo "Killing process $SOCAT_PID"
+        kill $SOCAT_PID
+    fi
+
+    exit 0
+fi 
+
+# If the device was added
+if [ "$ACTION" = "add" ]; then 
+    echo "Debugging is enabled. Creating a tunnel on serial port $DEVNAME"
+
+    if [ -e "$DEVNAME" ]; then 
+        echo Create TTY tunnel for "$DEVNAME"
+        socat $DEVNAME,raw,echo=0 /dev/hvc3,raw,echo=0 &        
+    else
+        echo "The device $DEVNAME does not exist"
+        exit 1
     fi 
-done
+fi
+
+exit 0
