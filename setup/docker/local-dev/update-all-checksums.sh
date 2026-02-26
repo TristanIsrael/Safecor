4606d3c9cd099790deed39f49bc3b19b49065879
Tristan Israël <5672131+TristanIsrael@users.noreply.github.com>
2026-02-26 18:29:31 +0100

    update docker scripts

--- setup/docker/local-dev/update-all-checksums.sh
+++ setup/docker/local-dev/update-all-checksums.sh
@@ -2,5 +2,5 @@
 
 PACKAGES="py3-paho-mqtt2 safecor-container-debian safecor-core safecor-demo safecor-demo-gui safecor-diag safecor-gui-base safecor-hardening safecor-lib safecor-splash safecor-sys-gui safecor-sys-usb safecor-syslog safecor-tests safecor-tests-gui"
 for pkg in $PACKAGES; do
-    ./run-docker.sh 'cd /home/builder/src/safecor/setup/packages/safecor/'$pkg' && abuild checksum && abuild clean'
+    ./docker-cmd.sh 'cd /home/builder/src/safecor/setup/packages/safecor/'$pkg' && abuild checksum && abuild clean'
 done
