d3c90b8f13b8b9f693ee09e3eadc8596476e16af
Tristan Israël <5672131+TristanIsrael@users.noreply.github.com>
2026-03-07 16:24:10 +0100

    #161

--- setup/docker/local-dev/update-all-checksums.sh
+++ setup/docker/local-dev/update-all-checksums.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PACKAGES="py3-paho-mqtt2 safecor-container-debian safecor-core safecor-demo safecor-demo-gui safecor-diag safecor-gui-base safecor-hardening safecor-lib safecor-splash safecor-sys-gui safecor-sys-usb safecor-syslog safecor-tests safecor-tests-gui"
+PACKAGES="py3-paho-mqtt2 safecor-container-debian safecor-core safecor-demo safecor-demo-gui safecor-diag safecor-gui-base safecor-hardening safecor-lib safecor-splash safecor-sys-gui safecor-sys-usb safecor-syslog safecor-tests safecor-tests-gui safecor-domu-base"
 for pkg in $PACKAGES; do
     ./docker-cmd.sh 'cd /home/builder/src/safecor/setup/packages/safecor/'$pkg' && abuild checksum && abuild clean'
 done
