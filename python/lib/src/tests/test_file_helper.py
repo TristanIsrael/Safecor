a98d31676a548af36437fff09ede978cad41b389
Tristan Israël <5672131+TristanIsrael@users.noreply.github.com>
2026-03-11 17:36:08 +0100

    Update test_file_helper.py

--- python/lib/src/tests/test_file_helper.py
+++ python/lib/src/tests/test_file_helper.py
@@ -7,7 +7,8 @@ class TestFileHelper(unittest.TestCase):
         filenames = [
             "A filename with ext.zip",
             "An archive ISO.iso",
-            "A complex filename-123.00.zip"
+            "A complex filename-123.00.zip",
+            "a file.name.schmoll-1.0.13.zip"
         ]
 
         for filename in filenames:
