9661d05d028a9564ff569e55da133e9a0f609ba2
Tristan Israël <5672131+TristanIsrael@users.noreply.github.com>
2026-07-15 21:53:51 +0200

    #212

--- setup/iso/tests/mkimg.tests.sh
+++ setup/iso/tests/mkimg.tests.sh
@@ -1,11 +1,88 @@
+build_xen() {
+    # XEN section
+	apk fetch --root "$APKROOT" --stdout xen-hypervisor | tar -C "$DESTDIR" -xz boot
+}
+
+build_splash() {
+    # Splash section 
+    local file="$1"
+    msg "Handling splash file $file"
+
+    local ext="${file##*.}"
+    ext=$(echo "$ext" | tr '[:upper:]' '[:lower:]')
+
+    if [ ! -f "$file" ]; then
+        msg "The splash file '$file' does not exist"
+        return 1
+    fi
+
+    if [ "$ext" != "ppm" ]; then
+        # Convert to PPM
+        local out="${file%.*}.ppm"
+        msg "Convert splash to PPM format"
+        magick convert "$file" "$out"
+        file=$out
+    fi
+
+    msg "Adding splash image $file to ISO at ${DESTDIR}"
+    cp $file ${DESTDIR}/fbsplash.ppm    
+
+    return $?
+}
+
+section_xen() {
+    # XEN section
+	[ -n "${xen_params+set}" ] || return 0
+	build_section xen $ARCH $(apk fetch --root "$APKROOT" --simulate xen-hypervisor | checksum)
+}
+
+section_splash() {
+    # Splash section
+    local _splash=""
+
+    for file in splash.ppm splash.png; do
+        [ -f "$file" ] && { msg "Splash file found: $file"; _splash=$file break; }
+    done
+
+    if [ ! -f "$PWD/$_splash" ]; then 
+        msg "Could not open $_splash in $PWD"
+        return 0 # Fail silently
+    fi
+
+    build_section splash "$PWD/$_splash"
+}
+
+profile_diag() {
+    profile_standard
+    
+    profile_abbrev="diag"
+    title="Safecor Diag"
+    desc="Diagnostic app for Safecor"
+    xen_params="quiet console=null loglevel=0"
+    kernel_cmdline="$kernel_cmdline console=null loglevel=0 autodetect_serial=no"
+    apks="$apks rng-tools xen xen-hypervisor syslinux safecor-diag"
+    apkovl="genapkovl-diag.sh"
+    hostname="safecor-diag"
+    boot_addons=""   
+    syslinux_serial=""
+    kernel_addons=""
+    kernel_flavors="lts"
+}
+
+
 profile_tests() {
     profile_safecor
 
     profile_abbrev="tests"
     title="Safecor tests"
     desc="Functional tests app for Safecor"
-    kernel_cmdline="$kernel_cmdline autodetect_serial=no"
-    apks="$apks safecor-tests"    
+    xen_params="quiet console=null loglevel=0"
+    kernel_cmdline="$kernel_cmdline console=null loglevel=0 autodetect_serial=no"
+    apks="$apks rng-tools xen xen-hypervisor syslinux safecor-tests"
+    apkovl="genapkovl-tests.sh"
     hostname="safecor-tests"
     boot_addons=""   
+    syslinux_serial=""
+    kernel_addons=""
+    kernel_flavors="lts"
 }
